a2e4aeb03dadc80836d59f54f5cbd001433ad581
sloanstewart <sloanstewartdev@gmail.com>
2017-12-29 16:42:40 -0500

    Setup simple form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+// import { fieldPropTypes } from 'redux-form/lib/propTypes';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
+      </div>
+    );
+  }
+
   render() {
-    return <div>PostsNew!</div>;
+    return (
+      <div>
+        <h3>Add a Post</h3>
+        <form>
+          <Field label="Title" name="title" component={this.renderField} />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field label="Content" name="content" component={this.renderField} />
+        </form>
+      </div>
+    );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || !values.content.length < 3) {
+    errors.title = 'Enter a valid title';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content || !values.content.length < 10) {
+    errors.content = 'Enter some content for your post';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostNewForm',
+})(PostsNew);
