55d70004463a5b71980e0549b461cf5efdfb84c1
sloanstewart <sloanstewartdev@gmail.com>
2017-12-29 14:26:37 -0500

    Now renders list of posts on posts_index

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <div>Post Index</div>;
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
-export default PostIndex;
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
